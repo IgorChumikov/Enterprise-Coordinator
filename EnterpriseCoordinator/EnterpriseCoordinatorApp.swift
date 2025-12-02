b4ad1e1002a0350b1b25e7c6791d358e524ca173
Igpr <chumikov.igor@mail.ru>
2025-12-02 16:18:40 +0700

    add

---
+++ EnterpriseCoordinator/EnterpriseCoordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnterpriseCoordinatorApp.swift
+//  EnterpriseCoordinator
+//
+//  Created by Игорь Чумиков on 02.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct EnterpriseCoordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
